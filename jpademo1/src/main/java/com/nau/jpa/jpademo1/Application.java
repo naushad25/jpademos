30a739966639d22a8f705c4df5d19f7fd3bb07b1
Naushad <naushad25@gmail.com>
2020-11-24 00:05:35 +0530

    First Commit

---
+++ jpademo1/src/main/java/com/nau/jpa/jpademo1/Application.java
@@ -0,0 +1,26 @@
+package com.nau.jpa.jpademo1;
+
+import java.sql.SQLException;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Application {
+
+    public static void main(String[] args) throws SQLException {
+
+        final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("myapp");
+        final EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        Customer customer = new Customer();
+        customer.setFirstName("Naushad");
+        customer.setLastName("Akhtar Khan");
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(customer);
+        entityManager.getTransaction().commit();
+
+    }
+
+  }
